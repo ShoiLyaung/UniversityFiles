443c022bf9218b335483c10fd00b2ccc1bb257d9
ShoiLyaung <2596985510@qq.com>
2023-10-18 09:38:28 +0800

    vault backup: 2023-10-18 09:38:28

--- Third/Fall/OS/Lab/week8/p.c
+++ Third/Fall/OS/Lab/week8/p.c
@@ -9,7 +9,7 @@ int main(){
     int fd;
     fd=open("./p.c",O_RDONLY);
     off_t len=lseek(fd,0,SEEK_END);
-    lseek(fd,0,SEEK_SET);
+//    lseek(fd,0,SEEK_SET);
     char *buffer=malloc(len);
     read(fd,buffer,len);
     printf("%s\n",buffer);
