52aaf8c8e6196be4608e996a3d50cf244d73bf98
ShoiLyaung <2596985510@qq.com>
2023-11-26 20:57:03 +0800

    运筹与优化 实验1作业1

--- Third/Winter/ORO/Lab/week1/task1.m
+++ Third/Winter/ORO/Lab/week1/task1.m
@@ -1,71 +1,29 @@
 m = 3;
 n = 8;
 
-A = 10 * rand(m, n);
-% disp('生成的矩阵 A:');
-% disp(A);
-
-I = eye(m, m);
-% disp('生成的矩阵 I:');
-% disp(I);
-
-randIndex = randperm(size(A, 2));
-% disp(randIndex);
-A(:, randIndex(1:m)) = I;
-disp('生成的矩阵 A:');
-disp(A);
-
-
-% 
-% % 判断是否存在单位矩阵
-% isUnitMatrix = isequal(A(:, randIndex(1:m)), I);
-% 
-% if isUnitMatrix
-%     disp('存在单位矩阵');
-%     % 给出所在列
-%     columnIndices = randIndex(1:m);
-%     disp('单位矩阵所在列：');
-%     disp(columnIndices);
-% else
-%     disp('不存在单位矩阵');
-% end
-
-foundUnitMatrix = false;
-columnIndices = [];
-
-% % 仅靠考虑m*m
-% for i = 1:(n - m + 1)
-%     if isequal(A(:, i:(i+m-1)), I)
-%         foundUnitMatrix = true;
-%         columnIndices = i:(i+m-1);
-%         break;
-%     end
-% end
-
-
-% 考虑1*1和2*2
-for i = 1:(n - m + 1)
-    for j = 1:(m - 1)
-        subMatrix = A(:, i:(i+j-1));
-        
-        % Check if the subMatrix is an identity matrix
-        if isequal(subMatrix, eye(size(subMatrix)))
-            foundUnitMatrix = true;
-            columnIndices = i:(i+j-1);
-            break;
+flag = 0;
+while flag < m
+    A = insertUnitMatrices(m, n);
+    for level = 1:m
+        unitMatrices = eye(level);
+        for i = 1:(n-level+1)
+            for j = 1:(m-level+1)
+                submatrix = A(j:j+level-1, i:i+level-1);
+                % 检查是否为单位矩阵
+                if isequal(submatrix, unitMatrices)
+                    disp(['在列 ', num2str(i), ' 发现阶数为 ', num2str(level), ' 的单位矩阵']);
+                    flag = max(flag, level);
+                end
+            end
         end
-    end
-    
-    if foundUnitMatrix
-        break;
     end
 end
 
-% 输出结果
-if foundUnitMatrix
-    disp('存在单位矩阵');
-    disp('单位矩阵所在列：');
-    disp(columnIndices);
-else
-    disp('不存在单位矩阵');
+function [A] = insertUnitMatrices(m, n)
+    A = 10 * rand(m, n);
+    I = eye(m, m);
+    randIndex = randperm(size(A, 2));
+    A(:, randIndex(1:m)) = I;
+    disp('生成的矩阵 A:');
+    disp(A);
 end
