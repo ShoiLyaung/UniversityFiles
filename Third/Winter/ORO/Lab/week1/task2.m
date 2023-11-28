e774178904662135b5855dcddf13e58e2d689903
ShoiLyaung <2596985510@qq.com>
2023-11-28 13:15:56 +0800

    运筹与优化第一周作业

--- Third/Winter/ORO/Lab/week1/task2.m
+++ Third/Winter/ORO/Lab/week1/task2.m
@@ -15,14 +15,14 @@ function x = gauss_elim(A, b)
 
     % 高斯消元
     for i = 1:n
-        % 列主元消元法，将当前列的主元调整为非零
+        % 将当前列的主元调整为非零
         if Ab(i, i) == 0
             [~, max_row] = max(abs(Ab(i:end, i)));
             max_row = max_row + i - 1;
             Ab([i, max_row], :) = Ab([max_row, i], :);
         end
 
-        % 将当前主元所在列的其他元素消为零
+        % 将当前列的其他元素消为零
         for j = i+1:n
             factor = Ab(j, i) / Ab(i, i);
             Ab(j, :) = Ab(j, :) - factor * Ab(i, :);
