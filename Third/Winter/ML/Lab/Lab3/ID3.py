2dcfca07e9e4bae0a0e1a134f9580120dc6cdf0e
ShoiLyaung <2596985510@qq.com>
2023-12-18 17:38:03 +0800

    机器学习 实验3 决策树

---
+++ Third/Winter/ML/Lab/Lab3/ID3.py
@@ -0,0 +1,8 @@
+import trees
+import treePlotter
+
+fr=open('lenses.txt')
+lenses=[inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels=['age','prescript','astigmatic','tearRate']
+lensesTree=trees.createTree(lenses,lensesLabels)
+treePlotter.createPlot(lensesTree)
