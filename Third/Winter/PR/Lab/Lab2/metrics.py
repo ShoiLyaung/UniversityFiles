343a1e71e9304870baf6cc093e2e9ce54031e01b
ShoiLyaung <2596985510@qq.com>
2023-12-27 14:21:49 +0800

    PR | Lab2 | Bayes Classfier: metrics

--- Third/Winter/PR/Lab/Lab2/metrics.py
+++ Third/Winter/PR/Lab/Lab2/metrics.py
@@ -1,17 +1,35 @@
-from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import metrics
+
+def accuracy_score(true_labels, predicted_labels):
+    correct_predictions = np.sum(true_labels == predicted_labels)
+    total_predictions = len(true_labels)
+    accuracy = correct_predictions / total_predictions
+    return accuracy
+
+def confusion_matrix(true_labels, predicted_labels):
+    pass
+
+def classification_report(true_labels, predicted_labels):
+    pass
 
 def get_metrics(true_labels, predicted_labels, predicted_score):
-    print("准确率为: {}".format(metrics.accuracy_score(true_labels, predicted_labels)))
-    print("混淆矩阵为:\n{}".format(metrics.confusion_matrix(true_labels, predicted_labels)))
-    print("分类报告为:\n{}".format(metrics.classification_report(true_labels, predicted_labels))) 
+    acc = accuracy_score(true_labels, predicted_labels)
+    conf_matrix = metrics.confusion_matrix(true_labels, predicted_labels)
+    cls_report = metrics.classification_report(true_labels, predicted_labels)
+    # ROC
     label_mapping = {'男': 1, '女': 0}
     true_labels = np.vectorize(label_mapping.get)(true_labels)
     predicted_labels = np.vectorize(label_mapping.get)(predicted_labels)
-    print("ROC曲线下面积为: {}".format(metrics.roc_auc_score(true_labels, predicted_score)))
-    # 绘制ROC曲线
+    roc_auc_score = metrics.roc_auc_score(true_labels, predicted_score)
     fpr, tpr, thresholds = metrics.roc_curve(true_labels, predicted_score)
+    
+    print("准确率为: {}".format(acc))
+    print("混淆矩阵为:\n{}".format(conf_matrix))
+    print("分类报告为:\n{}".format(cls_report)) 
+    print("ROC曲线下面积为: {}".format(roc_auc_score))
+    # 绘制ROC曲线
     plt.figure(figsize=(2, 2))
     plt.plot(fpr, tpr, label='ROC曲线')
     plt.plot([0, 1], [0, 1], linestyle='--', color='k', label='随机猜测')
